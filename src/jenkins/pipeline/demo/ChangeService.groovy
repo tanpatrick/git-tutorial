47b4b89eb2ae38e1b8e69f63f8d4ca9ce4cd4407
Patrick Tan <patrick.tan@vodafone.com>
2017-02-21 13:50:30 +1300

    Add function to retrieve modified dirs

--- src/jenkins/pipeline/demo/ChangeService.groovy
+++ src/jenkins/pipeline/demo/ChangeService.groovy
@@ -14,4 +14,26 @@ public class ChangeService implements Serializable {
 		this.steps = steps;
 		this.currentBuild = currentBuild;
 	}
+
+	def retrieveModifiedDirs() {
+		this.steps.echo 'Scanning for modified dirs on the current build...';
+
+		def changeLogSets = currentBuild.changeSets;
+
+		Set<String> dirs = new HashSet<String>();
+
+		for(def change : changeLogSets) {
+			def entries  = change.items;
+
+			for(def entry : entries) {
+				for(def path : entry.affectedPaths) {
+					dirs.add(path.substring(0, path.indexOf("/")));
+				}
+			}
+		}
+
+		this.steps.echo "Modified dirs: ${dirs}";
+
+		return dirs;
+	}
 }
